e337165981bda6f7256cf0cd2423aa882c5110fa
Reem Nawaf <sama22836@gmail.com>
2026-01-01 13:56:38 +0300

    My local changes

---
+++ ITHelpDesk/Migrations/20260101103219_AddFullNameToAspNetUsers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITHelpDesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFullNameToAspNetUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetUsers");
+        }
+    }
+}
